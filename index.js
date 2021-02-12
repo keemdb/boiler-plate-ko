63d4a2a5a6b3abac75d9dc82bd436688c1ab8e74
Dongbeom Kim <keemdb@gmail.com>
2021-02-13 03:14:01 +0900

    info sec

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://keemdb:test1234@boilerplate.v3hxs.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
 	useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected..'))
   .catch(err => console.log(err))
@@ -11,7 +20,21 @@ mongoose.connect('mongodb+srv://keemdb:test1234@boilerplate.v3hxs.mongodb.net/te
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! -a-')
+})
+
+
+app.post('/register', (req, res) => {
+	//회원 가입할때 필요한 정보들을 clientdㅔ서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+	const user = new User(req.body)
+
+	user.save((err, userInfo) => {
+		if(err) return res.json({ success: false, err})
+		return res.status(200).json({
+			success: true
+		})
+	})
 })
 
 app.listen(port, () => {
